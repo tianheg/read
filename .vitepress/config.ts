30afcd7afb7baf5a64b62eec1265d68a600a75a7
tianheg <i@tianheg.co>
2023-10-30 10:01:32 +0800

    Add pagefind search, some books

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vitepress"
+import { pagefindPlugin } from "vitepress-plugin-pagefind"
 
 export default defineConfig({
   title: "书Book",
@@ -7,12 +8,35 @@ export default defineConfig({
   cleanUrls: true,
 
   themeConfig: {
-    search: {
-      provider: "local"
-    },
-    
+    outline: [2, 3],
     sidebar:
     [
+      {
+        text: "2021",
+        collapsed: true,
+        items: [
+          { text: "高效能人士的七个习惯", link: "/2021/the-7-habits-of-highly-effective-people" },
+          { text: "阿西莫夫索引", link: "/2021/asimov" },
+          { text: "如何阅读一本书", link: "/2021/how-to-read-a-book" },
+          { text: "史蒂夫·乔布斯传", link: "/2021/steve-jobs" },
+          { text: "黑客与画家", link: "/2021/hackers-and-painters" },
+          { text: "如何成为一名程序员", link: "/2021/how-to-be-a-programmer" },
+          { text: "谈美", link: "/2021/tan-mei" },
+          { text: "那些忧伤的年轻人", link: "/2021/naxie-youshangde-nianqingren" },
+          { text: "查拉图斯特拉如是说", link: "/2021/thus-spoke-zarathustra" },
+          { text: "社会心理学", link: "/2021/social-psychology-book" },
+          { text: "神曲", link: "/2021/divine-comedy" },
+          { text: "道德经", link: "/2021/dao-de-jing" },
+          { text: "借我一生", link: "/2021/jiewo-yisheng" },
+          { text: "心经", link: "/2021/xinjing" },
+          { text: "SICP(JS ver.)", link: "/2021/sicpjs" },
+          { text: "醉步男", link: "/2021/zui-bu-nan" },
+          { text: "看海的人", link: "/2021/kanhaide-ren" },
+          { text: "朝花夕拾", link: "/2021/zhaohua-xishi" },
+          { text: "人性的枷锁", link: "/2021/of-human-bondage" },
+          { text: "刀锋", link: "/2021/the-razors-edge" },
+        ]
+      },
       {
         text: "2020",
         collapsed: true,
@@ -21,6 +45,10 @@ export default defineConfig({
           { text: "拉多之星", link: "/2020/la-duo-zhi-xing" },
           { text: "我生有涯愿无尽：梁漱溟自述文录", link: "/2020/wo-shengyouya-yuanwujin" },
           { text: "时间之书", link: "/2020/shijian-zhi-shu" },
+          { text: "未来世界的幸存者", link: "/2020/weilai-shijie-de-xingcunzhe" },
+          { text: "三体", link: "/2020/santi" },
+          { text: "程序员修炼之道", link: "/2020/the-pragmatic-programmer" },
+          { text: "一九八四", link: "/2020/nineteen-eighty-four" },
         ]
       },
       {
@@ -37,4 +65,18 @@ export default defineConfig({
       }
     ]
   },
+
+  vite:{
+    plugins:[pagefindPlugin({
+      btnPlaceholder: "搜索",
+      placeholder: "搜索文档",
+      emptyText: "空空如也",
+      heading: "共 {{searchResult}} 条结果",
+      customSearchQuery(input){
+        return input.replace(/[\u4e00-\u9fa5]/g, " $& ")
+          .replace(/\s+/g," ")
+          .trim()
+      }
+    })],
+  }
 })
---
+++ 2021/sicpjs.md
@@ -0,0 +1,3 @@
+https://sourceacademy.org/sicpjs/index
+
+Lisp ver. https://mitpress.mit.edu/9780262510875/structure-and-interpretation-of-computer-programs/
