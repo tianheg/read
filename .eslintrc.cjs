6ea850e2d1a2037656facebef881b7352f09b6d7
tianheg <i@tianheg.co>
2024-03-04 19:17:27 +0800

    add config to eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,39 +1,30 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
   },
-  "extends": [
-    "eslint:recommended"
-  ],
-  "overrides": [
+  extends: ["eslint:recommended"],
+  overrides: [
     {
-      "env": {
-        "node": true
+      env: {
+        node: true,
       },
-      "files": [
-        ".eslintrc.{js,cjs}"
-      ],
-      "parserOptions": {
-        "sourceType": "script"
-      }
-    }
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
   ],
-  "parserOptions": {
-    "ecmaVersion": "latest",
-      "parser": "@typescript-eslint/parser",
-      "sourceType": "module"
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  "plugins": [
-    "@typescript-eslint",
-      "vue"
-  ],
-  "rules": {
-    "indent": [ "error", 2 ],
-    "linebreak-style": [ "error", "unix" ],
-    "quotes": [ "error", "double" ],
-    "semi": [ "error", "never" ],
-    "object-curly-spacing": [ "error", "always" ],
-    "no-trailing-spaces": "error"
-  }
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "never"],
+    "object-curly-spacing": ["error", "always"],
+    "no-trailing-spaces": "error",
+  },
 }
