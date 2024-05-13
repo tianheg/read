a455961925215cc1a13af4ccc0b6799ed71bc989
tianheg <i@tianheg.co>
2024-05-13 23:05:39 +0800

    format files with biome

--- scripts/gen_book_list.mjs
+++ scripts/gen_book_list.mjs
@@ -1,34 +1,37 @@
-import config from "../.vitepress/config.js"
 import fs from "fs/promises";
+import config from "../.vitepress/config.js";
 
 (async () => {
   try {
     const bookList = config.themeConfig.sidebar
-      .map(({ text, link, score }) => `- [${":+1:".repeat(score)}${text}](${link})`)
-      .join("\n")
+      .map(
+        ({ text, link, score }) =>
+          `- [${":+1:".repeat(score)}${text}](${link})`,
+      )
+      .join("\n");
 
-    const filePath = "books/index.md"
-    const startIndex = "<!-- index start -->"
-    const endIndex = "<!-- index end -->"
+    const filePath = "src/index.md";
+    const startIndex = "<!-- index start -->";
+    const endIndex = "<!-- index end -->";
 
-    const data = await fs.readFile(filePath, "utf8")
+    const data = await fs.readFile(filePath, "utf8");
 
-    const startIndexPos = data.indexOf(startIndex)
-    const endIndexPos = data.indexOf(endIndex)
+    const startIndexPos = data.indexOf(startIndex);
+    const endIndexPos = data.indexOf(endIndex);
 
     if (startIndexPos === -1 || endIndexPos === -1) {
-      throw new Error("Start or end index not found in the file")
+      throw new Error("Start or end index not found in the file");
     }
 
     const updatedData = `${data.slice(
       0,
-      startIndexPos + startIndex.length
-    )}\n${bookList}\n${data.slice(endIndexPos)}`
+      startIndexPos + startIndex.length,
+    )}\n${bookList}\n${data.slice(endIndexPos)}`;
 
-    await fs.writeFile(filePath, updatedData, "utf8")
+    await fs.writeFile(filePath, updatedData, "utf8");
 
-    console.log("Book list added to index.md successfully")
+    console.log("Book list added to index.md successfully");
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
-})()
+})();
